8634488ccee30681747b05db14b9a356f8730af1
Ayanna Scoon-Jerome <ayanna.scoon@gmail.com>
2018-01-15 01:20:34 -0500

    complete inquirer calls, log text to file, add function for doing what the file says

--- liri.js
+++ liri.js
@@ -1,15 +1,10 @@
 require("dotenv").config();
 
 var fs = require("fs");
-
 var request = require("request");
-
 var Twitter = require("twitter");
-
 var Spotify = require('node-spotify-api');
-
 var keys = require("./keys.js");
-
 var inquirer = require("inquirer");
 
 //console.log("Keys: ", keys);
@@ -24,34 +19,71 @@ var userMovie = "";
 //console.log("Spotify: ", spotify)
 //console.log("Twitter: ", client);
 
-//var userSelection = process.argv[2];
-//var userValue = process.argv[3];
+startLiri();
 
+function startLiri() {
+    console.log("Welcome to the Liri Command Line Search App. You can look for movies, tweets and songs! ");
 
-inquirer.prompt([{
-    type: "list",
-    name: "activity",
-    message: "Liri is ready when you are",
-    choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
-}]).then(function (inqResponse) {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "start",
+        message: "Would you like to try it out?",
+        default: false
 
-    var activity = inqResponse.activity;
+    }]).then(function (inqResponse) {
 
-    //switch (userSelection) {
+        if (inqResponse.start === true) {
+            displayMenu();
+        } else {
+            console.log("============================================================");
+            console.log("Okay, maybe next time!");
+        }
+    });
+}
 
-    evaluateSelection(activity);
-});
+function displayMenu() {
+    console.log("============================================================");
+    inquirer.prompt([{
+        type: "list",
+        name: "activity",
+        message: "Liri is ready when you are",
+        choices: ["my-tweets", "spotify-this-song", "movie-this", "do-what-it-says"]
+    }]).then(function (inqResponse) {
+
+        console.log("============================================================");
+        var activity = inqResponse.activity;
+        evaluateSelection(activity);
+    });
+};
+
+function searchMore() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "searchAgain",
+        message: "Wanna look up more stuff?",
+        default: false
+
+    }]).then(function (inqResponse) {
+
+        if (inqResponse.searchAgain === true) {
+            displayMenu();
+        } else {
+            console.log("============================================================");
+            console.log("Thanks for trying Liri! Come back soon.");
+        }
+    });
+}
 
 function evaluateSelection(action) {
 
-
+    logThis(action);
     switch (action) {
         case "my-tweets":
-            getTweets(twitHandle);
+            getTwitHandle();
             break;
 
         case "spotify-this-song":
-            getSongInfo(songChoice);
+            getSong();
             break;
 
         case "movie-this":
@@ -62,10 +94,30 @@ function evaluateSelection(action) {
             doWhatItSays();
             break;
     };
-    
+
 }
 
-function getTweets(userScreenName) {
+
+function evaluateFile(action, option) {
+
+    // fs.appendFileSync("./log.txt",action);
+    switch (action) {
+        case "my-tweets":
+            getTweets(option);
+            break;
+
+        case "spotify-this-song":
+            getSongInfo(option);
+            break;
+
+        case "movie-this":
+            getMovieInfo(option);
+            break;
+    };
+
+}
+
+function getTwitHandle() {
 
     inquirer.prompt([{
         type: "input",
@@ -75,54 +127,81 @@ function getTweets(userScreenName) {
     }]).then(function (inqResponse) {
 
         userScreenName = inqResponse.userScreenName;
-
-        var params = {
-            screen_name: userScreenName,
-            count: 5
-        };
-
-        client.get('statuses/user_timeline', params, function (err, data) {
-            //console.log(data);
-            for (var i = 0; i < data.length; i++) {
-                console.log("============================================================\n");
-                console.log("Tweet #" + (i + 1) + ": " + data[i].text);
-                // console.log("\n");
-            };
-            console.log("============================================================");
-        });
-
+        getTweets(userScreenName);
     });
 
+}
+
+function getTweets(twitHandle) {
+
+    var params = {
+        screen_name: twitHandle,
+        count: 5
+    };
+
+    client.get('statuses/user_timeline', params, function (err, data) {
+        //console.log(data);
+        console.log(data.length);
+        if (data.length === 0 || typeof data.length === "undefined") {
+            logThis("No tweets here. Make sure you've spelled the screen-name correctly.")
+            displayMenu();
+        } else {
+            data.forEach(function (element, i) {
+                logThis("============================================================\n");
+                logThis("Tweet #" + (i + 1) + ": " + element.text);
+                logThis("Created On: " + element.created_at);
+            });
+            logThis("============================================================");
+            searchMore();
+        }
+
+
+        // for (var i = 0; i < data.length; i++) {
+        //     console.log("============================================================\n");
+        //     console.log("Tweet #" + (i + 1) + ": " + data[i].text);
+        //     // console.log("\n");
+        // };
+        // console.log("============================================================");
+        // searchMore();
+    });
 };
 
 
 
-function getSongInfo(userSong) {
+function getSong() {
 
     inquirer.prompt([{
         type: "input",
         name: "userSong",
         message: "Enter a song to look up: ",
-        default: "The Sign"
+        default: "The Sign by Ace of Base"
     }]).then(function (inqResponse) {
 
         userSong = inqResponse.userSong;
+        console.log("This is the song I want to search for: ", userSong);
+        getSongInfo(userSong);
+    });
 
-        spotify.search({
-            type: 'track',
-            query: userSong,
-            limit: 5
-        }, function (err, data) {
-            if (err) {
-                return console.log('Error occurred: ' + err);
-            }
+};
 
+function getSongInfo(song) {
+
+    console.log("Is this my song: ", song);
+
+    spotify.search({
+        type: 'track',
+        query: song,
+        limit: 5
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        } else {
             var songResults = data.tracks.items;
-
+            logThis("You searched for: " + song);
             //console.log("==================" + songName + "==================");
             for (let i = 0; i < songResults.length; i++) {
                 // const element = songResults[i];
-                console.log("========================================================================================================================= \n");
+                logThis("========================================================================================================================= \n");
                 // console.log("Artists:")
                 var songArtists = [];
                 for (let j = 0; j < songResults[i].artists.length; j++) {
@@ -130,10 +209,10 @@ function getSongInfo(userSong) {
                     var listOfArtists = songArtists.join(", ");
                 };
 
-                console.log("Song Name: ", songResults[i].name);
-                console.log("Artists: ", listOfArtists);
-                console.log("Preview URL: ", songResults[i].preview_url);
-                console.log("Album Name: ", songResults[i].album.name);
+                logThis("Song Name: " + songResults[i].name);
+                logThis("Artists: " + listOfArtists);
+                logThis("Preview URL: " + songResults[i].preview_url);
+                logThis("Album Name: " + songResults[i].album.name);
 
             };
             // console.log(data.tracks.items[0].artists[0].name);
@@ -144,11 +223,13 @@ function getSongInfo(userSong) {
 
             //console.log(JSON.stringify(data, null, 2));
             //console.log(data[0].name);
-            console.log("=========================================================================================================================");
-        });
-
+            logThis("=========================================================================================================================");
+            searchMore();
+        }
 
     });
+
+
 };
 
 
@@ -164,66 +245,81 @@ function getMovie() {
         userMovie = inqResponse.userMovie;
 
         getMovieInfo(userMovie);
- 
-    });
 
+    });
 };
 
 function getMovieInfo(movie) {
 
-// Then run a request to the OMDB API with the movie specified
-var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
+    // Then run a request to the OMDB API with the movie specified
+    var queryUrl = "http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&apikey=trilogy";
 
-console.log(queryUrl);
+    //console.log(queryUrl);
 
-// Then create a request to the queryUrl
-request(queryUrl, function (error, response, body) {
+    // Then create a request to the queryUrl
+    request(queryUrl, function (error, response, body) {
+        var {
+            Title,
+            Year
+        } = JSON.parse(body);
+        // If the request is successful (i.e. if the response status code is 200)
+        if (!error && response.statusCode === 200) {
 
-    // If the request is successful (i.e. if the response status code is 200)
-    if (!error && response.statusCode === 200) {
+            // Parse the body of the site and recover just the imdbRating
+            // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+            if (typeof JSON.parse(body).Year === "undefined") {
+                logThis("No movies by that name. Looks like you might have misspelled it :-(");
+                displayMenu();
+            } else {
+                logThis("++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                logThis("* Movie Title: " + Title);
+                logThis("* Release Year: " + JSON.parse(body).Year);
+                logThis("* IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
+                logThis("* Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+                logThis("* Country of Production: " + JSON.parse(body).Country);
+                logThis("* Language: " + JSON.parse(body).Language);
+                logThis("* Plot: " + JSON.parse(body).Plot);
+                logThis("* Actors: " + JSON.parse(body).Actors);
+                logThis("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
 
-        // Parse the body of the site and recover just the imdbRating
-        // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
-        console.log("* Movie Title: " + JSON.parse(body).Title);
-        console.log("* Release Year: " + JSON.parse(body).Year);
-        console.log("* IMDB Rating: " + JSON.parse(body).Ratings[0].Value);
-        console.log("* Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
-        console.log("* Country of Production: " + JSON.parse(body).Country);
-        console.log("* Language: " + JSON.parse(body).Language);
-        console.log("* Plot: " + JSON.parse(body).Plot);
-        console.log("* Actors: " + JSON.parse(body).Actors);
-    }
-});
+                searchMore();
+            }
+        }
+    });
 }
-
-
 
 
 function doWhatItSays() {
 
-    fs.readFile("random.txt", "utf8", function(error, data) {
+    fs.readFile("random.txt", "utf8", function (error, data) {
 
         if (error) {
             return console.log(error);
-          }
-        
-          // We will then print the contents of data
-          console.log(data);
-        
-          // Then split it by commas (to make it more readable)
-          var dataArr = data.split(",");
-        
-          // We will then re-display the content as an array for later use.
-          console.log(dataArr);
+        }
 
-          var fileAction = dataArr[0];
-          var searchTerm = dataArr[1];
+        // We will then print the contents of data
+        //console.log(data);
 
-          console.log(fileAction);
-          console.log(searchTerm);
+        // Then split it by commas (to make it more readable)
+        var dataArr = data.split(",");
 
-          evaluateSelection(fileAction, searchTerm, searchTerm, searchTerm);
+        // We will then re-display the content as an array for later use.
+        // console.log(dataArr);
+
+        var fileAction = dataArr[0];
+        var searchTerm = dataArr[1];
+
+        logThis(fileAction + ": " + searchTerm);
+        // console.log(searchTerm);
+
+        evaluateFile(fileAction, searchTerm);
+
     });
 
 
+}
+
+function logThis(message) {
+    console.log(message);
+    fs.appendFileSync("./log.txt", message + "\n", "utf8");
 }
